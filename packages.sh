b99460f967d202e0fc9f8d51fb19dba43d645b95
daicho <daichimax78@gmail.com>
2019-04-09 22:01:36 +0900

    gitを追加

--- packages.sh
+++ packages.sh
@@ -1,2 +1,2 @@
 sudo apt-get update
-sudo apt-get install chromium-browser-l10n default-jdk easystroke git-flow ibus-mozc mozc-utils-gui imwheel libopencv-dev texlive-lang-japanese v4l-utils vim vlc -y
+sudo apt-get install chromium-browser-l10n default-jdk easystroke git git-flow ibus-mozc mozc-utils-gui imwheel libopencv-dev texlive-lang-japanese v4l-utils vim vlc -y
